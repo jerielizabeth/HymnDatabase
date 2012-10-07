119dc818cb3162f26225a9dde5640c29c55cd576
Jeri Wieringa <jeri.elizabeth@gmail.com>
2012-10-06 21:34:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HymnDatabase::Application.config.secret_token = 'b4c915d2f2e1fa9f9242f5b9e41a11f6530a4de0edfebefbfe60b377942a8844800d77835c0f0c27cd843ba9deaa6c478ec7845c2e2842ed31927eb8f1dc2e73'
